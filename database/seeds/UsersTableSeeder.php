0a848e1d91552e4d05b0724cabf07478b8057930
m0stafa7med <mostafa.ahmed1093@yahoo.com>
2020-06-02 19:26:17 +0200

    first commit

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use App\User;
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = DB::table('users')->where('email', 'mostafadarwesh610@gmail.com')->first();
+
+        if (! $user) {
+            User::create([
+              'name' => 'Mustafa',
+              'email' => 'mostafadarwesh610@gmail.com',
+              'password' => Hash::make('123456'),
+              'role' => 'admin'
+            ]);
+          }
+    
+    
+    }
+}
